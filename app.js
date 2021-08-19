110396fcabd39abb0e4837f23867941cf225fcd9
Tamim <farman.arefin1@outlook.com>
2021-08-19 20:52:50 +0600

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//This is function is for getting a pin
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+//This function for showing the pin
+
+function generatePin() {
+    document.getElementById('show-pin').value = getPin();
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    console.log('Clicked');
+});
